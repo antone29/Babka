9e7574118f830e43fe52857f7c290447fccbdb59
Bekki Antonelli <antonelli017@gmail.com>
2024-06-06 10:54:41 -0400

    Initial Commit

---
+++ Babka/BabkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabkaApp.swift
+//  Babka
+//
+//  Created by Bekki Antonelli on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BabkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
