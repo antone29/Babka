7e4c24519d000a878814419f2f00df4cc44224b3
Bekki Antonelli <antonelli017@gmail.com>
2024-08-08 10:48:32 -0400

    transactions population on transactions page

---
+++ Babka/Models/BankModel.swift
@@ -0,0 +1,13 @@
+//
+//  BankModel.swift
+//  Babka
+//
+//  Created by Bekki Antonelli on 8/7/24.
+//
+
+import Foundation
+
+
+class BankModel: Codable, ObservableObject, Identifiable {
+    
+}
---
+++ Babka/Models/BanksDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  BanksDataModel.swift
+//  Babka
+//
+//  Created by Bekki Antonelli on 8/7/24.
+//
+
+import Foundation
+
+//
+//class BanksDataModel: ObservableObject {
+//    @Published var bankList: Bank
+//}
